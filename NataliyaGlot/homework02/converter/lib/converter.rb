f118c3d3347ef30531809c189eec6e82f6bc03d4
Nataliya <an.chishevich@yandex.ru>
2020-08-03 22:04:10 +0300

    My first converter

--- NataliyaGlot/homework02/converter/lib/converter.rb
+++ NataliyaGlot/homework02/converter/lib/converter.rb
@@ -1,85 +1,96 @@
 require 'money'
 require 'json'
 require 'colorize'
+require 'rubocop'
+
 class Converter
   
-    attr_accessor :num_conv, :cur_name
+  attr_accessor :num_conv, :cur_name
 
-    def initialize(num_conv, cur_name)
-      file = File.read('converter.json')
-      @file_hash = JSON.parse(file) # use absolute path
-      @num_conv = num_conv
-      @cur_name = cur_name
-    end
+  def initialize(num_conv, cur_name)
+    current_path = File.dirname(__FILE__)
+    file_path = File.read(current_path + "/converter.json")
+    @file_hash = JSON.parse(file_path) 
+    @num_conv = num_conv
+    @cur_name = cur_name
+    @cur_rate_eur = @file_hash["currency"][0]["cur_rate"]
+    @cur_rate_usd = @file_hash["currency"][1]["cur_rate"]
+    @cur_scale_rus = @file_hash["currency"][2]["cur_scale"]
+    @cur_rate_rus = @file_hash["currency"][2]["cur_rate"]
+  end
 
-    def to_eur
-      @cur_scale = @file_hash["currency"][0]["cur_scale"]
-      @cur_rate = @file_hash["currency"][0]["cur_rate"]
-      (@num_conv * @cur_scale / @cur_rate).round(2) # must be integer
+  def to_eur
+    if cur_name == "BYN"
+      (@num_conv.to_i / @cur_rate_eur).round(2)
+    elsif cur_name == "USD"
+      (@cur_rate_usd / @cur_rate_eur).round(2)
+    elsif cur_name == "RUS"
+      (@cur_rate_rus / @cur_scale_rus * @cur_rate_eur).round(2)
+    else
+      "Currency not found"
     end
+  end
 
-    def to_usd
-      @cur_scale = @file_hash["currency"][1]["cur_scale"]
-      @cur_rate = @file_hash["currency"][1]["cur_rate"]
-      (@num_conv * @cur_scale / @cur_rate).round(2)
+  def to_usd
+    if cur_name == "BYN"
+      (@num_conv.to_i / @cur_rate_usd).round(2)
+    elsif cur_name == "EUR"
+      (@cur_rate_eur / @cur_rate_usd).round(2)
+    elsif cur_name == "RUS"
+      (@cur_rate_rus / @cur_scale_rus * @cur_rate_usd).round(2)
+    else
+      "Currency not found"
     end
+  end
 
-    def to_rus
-      @cur_scale = @file_hash["currency"][2]["cur_scale"]
-      @cur_rate = @file_hash["currency"][2]["cur_rate"]
-      (@num_conv * @cur_scale / @cur_rate).round(2)
+  def to_rus
+    if cur_name == "BYN"
+      (@num_conv.to_i * @cur_scale_rus / @cur_rate_rus).round(2)
+    elsif cur_name == "USD"
+      (@cur_rate_usd * @cur_scale_rus / @cur_rate_rus).round(2)
+    elsif cur_name == "EUR"
+      (@cur_rate_eur * @cur_scale_rus / @cur_rate_rus).round(2)
+    else
+      "Currency not found"
     end
+  end
 
-    def to_byn
-      if @cur_name == "EUR" 
-        @cur_scale = @file_hash["currency"][0]["cur_scale"]
-        @cur_rate = @file_hash["currency"][0]["cur_rate"]
-        (@num_conv / @cur_scale * @cur_rate).round(2)
-      elsif @cur_name == "USD"
-        @cur_scale = @file_hash["currency"][1]["cur_scale"]
-        @cur_rate = @file_hash["currency"][1]["cur_rate"]
-        (@num_conv / @cur_scale * @cur_rate).round(2)
-      elsif @cur_name == "RUS"
-        @cur_scale = @file_hash["currency"][2]["cur_scale"]
-        @cur_rate = @file_hash["currency"][2]["cur_rate"]
-        (@num_conv / @cur_scale * @cur_rate).round(2)
-      else
-        "Currency not found"
-      end
+  def to_byn
+    if @cur_name == "EUR" 
+      (@num_conv.to_i * @cur_rate_eur).round(2)
+    elsif @cur_name == "USD"
+      (@num_conv.to_i * @cur_rate_usd).round(2)
+    elsif @cur_name == "RUS" && @num_conv >= '100'
+      (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus).round(2)
+    else
+      "Currency not found or must be >= 100"
     end
+  end
 
-    def self.convert(num_conv, cur_from, cur_to)
-      file = File.read('converter.json')
-      @file_hash = JSON.parse(file)
-      @num_conv = num_conv
-      @cur_from = cur_from
-      @cur_to = cur_to
-        if @cur_from == 'USD' && @cur_to == 'EUR'
-          @cur_scale = @file_hash["currency"][1]["cur_scale"]
-          @cur_rate = @file_hash["currency"][1]["cur_rate"]
-          ((@num_conv / @cur_scale * @cur_rate) / @file_hash["currency"][0]["cur_rate"]).round(2)
-        elsif @cur_from == 'EUR' && @cur_to == 'USD'
-          @cur_scale = @file_hash["currency"][0]["cur_scale"]
-          @cur_rate = @file_hash["currency"][0]["cur_rate"]
-          (@num_conv / @cur_scale * @cur_rate) / @file_hash["currency"][1]["cur_rate"].round(2)
-        elsif @cur_from == 'EUR' && @cur_to == 'RUS'
-          @cur_scale = @file_hash["currency"][0]["cur_scale"]
-          @cur_rate = @file_hash["currency"][0]["cur_rate"]
-          (@num_conv / @cur_scale * @cur_rate) * @file_hash["currency"][2]["cur_scale"] / @file_hash["currency"][2]["cur_rate"].round(2)
-        elsif @cur_from == 'RUS' && @cur_to == 'EUR'
-          @cur_scale = @file_hash["currency"][2]["cur_scale"]
-          @cur_rate = @file_hash["currency"][2]["cur_rate"]
-          (@num_conv / @cur_scale * @cur_rate) / @file_hash["currency"][0]["cur_rate"].round(2)
-        elsif @cur_from == 'USD' && @cur_to == 'RUS'
-          @cur_scale = @file_hash["currency"][1]["cur_scale"]
-          @cur_rate = @file_hash["currency"][1]["cur_rate"]
-          (@num_conv / @cur_scale * @cur_rate) / @file_hash["currency"][2]["cur_scale"] / @file_hash["currency"][2]["cur_rate"].round(2)
-        elsif @cur_from == 'RUS' && @cur_to == 'USD'
-          @cur_scale = @file_hash["currency"][2]["cur_scale"]
-          @cur_rate = @file_hash["currency"][2]["cur_rate"]
-          (@num_conv / @cur_scale * @cur_rate) / @file_hash["currency"][1]["cur_rate"].round(2)
-        else
-          "Currency not found"
-        end
+  def self.convert(num_conv, cur_from, cur_to)
+    current_path = File.dirname(__FILE__)
+    file_path = File.read(current_path + "/converter.json")
+    @file_hash = JSON.parse(file_path)
+    @num_conv = num_conv
+    @cur_from = cur_from
+    @cur_to = cur_to
+
+    if @cur_from == 'USD' && @cur_to == 'EUR'
+      (@num_conv.to_i * @cur_rate_usd / @cur_rate_eur).round(2)
+    elsif @cur_from == 'EUR' && @cur_to == 'USD'
+      (@num_conv.to_i * @cur_rate_eur / @cur_rate_usd).round(2)
+    elsif @cur_from == 'EUR' && @cur_to == 'RUS'
+      (@num_conv.to_i * @cur_rate_eur * @cur_scale_rus / @cur_rate_rus).round(2)
+    elsif @cur_from == 'RUS' && @cur_to == 'EUR'
+      (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus / @cur_rate_rus).round(2)
+    elsif @cur_from == 'USD' && @cur_to == 'RUS'
+      (@num_conv.to_i * @cur_rate_usd / @cur_scale_rus / @cur_rate_rus).round(2)
+    elsif @cur_from == 'RUS' && @cur_to == 'USD'
+      (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus / @cur_rate_usd).round(2)
+    else
+      "Currency not found"
     end
+  end
 end
+converter = Converter.new(1.00, 'RUS')
+puts converter.to_byn
--- NataliyaGlot/homework02/main/test.rb
+++ NataliyaGlot/homework02/main/test.rb
@@ -2,11 +2,8 @@ require 'converter'
 require 'pry'
 
 converter = Converter.new('1.00', 'USD')
-	puts converter.to_eur # => Пример вывода: '$1 = €0.87 (<здесь информация про конвертируемые валюты>)'
-	puts converter.to_byn
-	puts converter.to_rub
+puts converter.to_eur 
+puts converter.to_byn
+puts converter.to_rub
 
-	# Допустимы другие варианты реализации, к примеру:
-	puts Converter.convert('1.00', 'USD', 'EUR') # amount, currency_from, currency_to
-	# => Пример вывода: '$1 = €0.87 (<здесь информация про конвертируемые валюты>)'
-
+puts Converter.convert('1.00', 'USD', 'EUR')
