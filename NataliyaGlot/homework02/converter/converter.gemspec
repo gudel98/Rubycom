331ed5239188d6d6c238ffc8c93fbeb8c68cf9ff
Nataliya <an.chishevich@yandex.ru>
2020-08-05 23:44:07 +0300

    Added some exceptions

--- NataliyaGlot/homework02/converter/converter.gemspec
+++ NataliyaGlot/homework02/converter/converter.gemspec
@@ -2,12 +2,12 @@ Gem::Specification.new do |s|
   s.name        = 'converter'
   s.version     = '0.0.0'
   s.date        = '2010-04-28'
-  s.summary     = "Converter!"
-  s.description = "A simple hello world gem"
-  s.authors     = ["NataliyaGlot"]
+  s.summary     = 'Converter!'
+  s.description = 'A simple hello world gem'
+  s.authors     = ['NataliyaGlot']
   s.email       = 'nick@quaran.to'
-  s.files       = ["lib/converter.rb"]
+  s.files       = ['lib/converter.rb']
   s.homepage    =
     'https://rubygems.org/gems/converter'
-  s.license       = 'MIT'
+  s.license = 'MIT'
 end
--- NataliyaGlot/homework02/converter/lib/converter.rb
+++ NataliyaGlot/homework02/converter/lib/converter.rb
@@ -1,75 +1,82 @@
-require 'money'
 require 'json'
 require 'colorize'
+require 'money'
 require 'rubocop'
 
 class Converter
-  
   attr_accessor :num_conv, :cur_name
 
   def initialize(num_conv, cur_name)
-    current_path = File.dirname(__FILE__)
-    file_path = File.read(current_path + "/converter.json")
-    @file_hash = JSON.parse(file_path) 
     @num_conv = num_conv
     @cur_name = cur_name
-    @cur_rate_eur = @file_hash["currency"][0]["cur_rate"]
-    @cur_rate_usd = @file_hash["currency"][1]["cur_rate"]
-    @cur_scale_rus = @file_hash["currency"][2]["cur_scale"]
-    @cur_rate_rus = @file_hash["currency"][2]["cur_rate"]
+    begin
+      current_path = File.dirname(__FILE__)
+      file_path = File.read(current_path + '/converter.json')
+    rescue StandardError
+      puts 'File not found or damaged'
+    end
+    @file_hash = JSON.parse(file_path)
+    @cur_rate_eur = @file_hash['currency'][0]['cur_rate']
+    @cur_rate_usd = @file_hash['currency'][1]['cur_rate']
+    @cur_scale_rus = @file_hash['currency'][2]['cur_scale']
+    @cur_rate_rus = @file_hash['currency'][2]['cur_rate']
+    if cur_name.nil? || cur_name == '' || cur_name.is_a?(Integer) || cur_name == cur_name.downcase
+      raise ArgumentError, 'Currency name entered incorrectly'
+    end
+    if num_conv.nil? || num_conv == '' || num_conv.is_a?(Integer)
+      raise ArgumentError, 'Currency amount entered incorrectly'
+    end
   end
 
   def to_eur
-    if cur_name == "BYN"
+    if cur_name == 'BYN'
       (@num_conv.to_i / @cur_rate_eur).round(2)
-    elsif cur_name == "USD"
-      (@cur_rate_usd / @cur_rate_eur).round(2)
-    elsif cur_name == "RUS"
-      (@cur_rate_rus / @cur_scale_rus * @cur_rate_eur).round(2)
+    elsif cur_name == 'USD'
+      (@num_conv.to_i * @cur_rate_usd / @cur_rate_eur).round(2)
+    elsif cur_name == 'RUS'
+      (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus / @cur_rate_eur).round(2)
     else
-      "Currency not found"
+      'Currency not found'
     end
   end
 
   def to_usd
-    if cur_name == "BYN"
+    if cur_name == 'BYN'
       (@num_conv.to_i / @cur_rate_usd).round(2)
-    elsif cur_name == "EUR"
-      (@cur_rate_eur / @cur_rate_usd).round(2)
-    elsif cur_name == "RUS"
-      (@cur_rate_rus / @cur_scale_rus * @cur_rate_usd).round(2)
-    else
-      "Currency not found"
+    elsif cur_name == 'EUR'
+      (@num_conv.to_i * @cur_rate_eur / @cur_rate_usd).round(2)
+    elsif cur_name == 'RUS'
+      (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus / @cur_rate_usd).round(2)
     end
   end
 
   def to_rus
-    if cur_name == "BYN"
+    if cur_name == 'BYN'
       (@num_conv.to_i * @cur_scale_rus / @cur_rate_rus).round(2)
-    elsif cur_name == "USD"
-      (@cur_rate_usd * @cur_scale_rus / @cur_rate_rus).round(2)
-    elsif cur_name == "EUR"
-      (@cur_rate_eur * @cur_scale_rus / @cur_rate_rus).round(2)
+    elsif cur_name == 'USD'
+      (@num_conv.to_i * @cur_rate_usd * @cur_scale_rus / @cur_rate_rus).round(2)
+    elsif cur_name == 'EUR'
+      (@num_conv.to_i * @cur_rate_eur * @cur_scale_rus / @cur_rate_rus).round(2)
     else
-      "Currency not found"
+      'Currency not found'
     end
   end
 
   def to_byn
-    if @cur_name == "EUR" 
+    if @cur_name == 'EUR'
       (@num_conv.to_i * @cur_rate_eur).round(2)
-    elsif @cur_name == "USD"
+    elsif @cur_name == 'USD'
       (@num_conv.to_i * @cur_rate_usd).round(2)
-    elsif @cur_name == "RUS" && @num_conv >= '100'
+    elsif @cur_name == 'RUS' && @num_conv.to_i >= 100
       (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus).round(2)
     else
-      "Currency not found or must be >= 100"
+      'Currency not found or must be >= 100'
     end
   end
 
   def self.convert(num_conv, cur_from, cur_to)
     current_path = File.dirname(__FILE__)
-    file_path = File.read(current_path + "/converter.json")
+    file_path = File.read(current_path + '/converter.json')
     @file_hash = JSON.parse(file_path)
     @num_conv = num_conv
     @cur_from = cur_from
@@ -84,13 +91,12 @@ class Converter
     elsif @cur_from == 'RUS' && @cur_to == 'EUR'
       (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus / @cur_rate_rus).round(2)
     elsif @cur_from == 'USD' && @cur_to == 'RUS'
-      (@num_conv.to_i * @cur_rate_usd / @cur_scale_rus / @cur_rate_rus).round(2)
+      (@num_conv.to_i * @cur_rate_usd * @cur_scale_rus / @cur_rate_rus).round(2)
     elsif @cur_from == 'RUS' && @cur_to == 'USD'
       (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus / @cur_rate_usd).round(2)
     else
-      "Currency not found"
+      'Currency not found'
     end
   end
 end
-converter = Converter.new(1.00, 'RUS')
-puts converter.to_byn
+
